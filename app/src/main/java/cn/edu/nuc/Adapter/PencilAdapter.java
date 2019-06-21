44dc9dba46918b068a473a4376978a6fd75b9a81
JanuaryZeng <15841331413@163.com>
2019-06-21 22:30:10 +0800

    1.1.4

---
+++ app/src/main/java/cn/edu/nuc/Adapter/PencilAdapter.java
@@ -0,0 +1,6 @@
+package cn.edu.nuc.Adapter;
+
+import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
+
+public class PencilAdapter extends BaseMultiItemQuickAdapter {
+}
---
+++ app/src/main/java/cn/edu/nuc/DataBase/PencilNote.java
@@ -0,0 +1,6 @@
+package cn.edu.nuc.DataBase;
+
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+
+public class PencilNote implements MultiItemEntity {
+}
